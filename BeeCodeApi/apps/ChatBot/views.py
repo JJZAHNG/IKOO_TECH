7919c0ef2644844bf736f3dccf005d351850989f
JJZAHNG <Jeazhang828@sina.com>
2024-03-26 22:38:16 +0800

    daily update
    daily update

--- BeeCodeApi/apps/ChatBot/views.py
+++ BeeCodeApi/apps/ChatBot/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import eurine4
+from apps.ChatBot import eurine4
 
 class BotChat(APIView):
     def post(self, request, *args, **kwargs):
